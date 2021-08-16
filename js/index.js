ddb170d8071b1301834d1334cbcb09f2ab5e52d5
Khurshida-Meem <meemrumi59@gmail.com>
2021-08-17 02:12:09 +0600

    changed title

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById('login-submit').addEventListener('click',function () {
+document.getElementById('login-submit').addEventListener('click', function () {
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
     const passwordField = document.getElementById('user-password');
